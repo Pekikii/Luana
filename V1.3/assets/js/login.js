9e31478c68f9e9cab47f4721ea041aad9fee7787
Vlad Pecheanu <Vlad.Pecheanu@gmail.com>
2019-06-19 20:14:38 +0200

    Progress on reading from database

--- V1.3/assets/js/login.js
+++ V1.3/assets/js/login.js
@@ -1,66 +1,58 @@
 
-const auth = firebase.auth();
-var db = firebase.firestore();
-var docRef = db.collection("items").doc("House 1");
-var mail;
+(function($) {
+	const auth = firebase.auth();
+	var mail;
 
-auth.onAuthStateChanged(function(user) {
-  if (user) {
-	if(window.location.href =="file:///D:/Programming%20etc/Shared%20Folder%20Ubuntu%20Main%2018.04.2/Luana/V1.3/login.html"){
-	mail = user.email;
-	window.alert("on login logged in. redirecting to index User Email: " + mail);
-	window.location.replace ("index.html");
-
-	}
-  } else {
-		docRef.get().then(function(doc) {
-			if (doc.exists) {
-				console.log("Document data:", doc.data());
-			} else {
-				// doc.data() will be undefined in this case
-				console.log("No such document!");
-			}
-		}).catch(function(error) {
-			console.log("Error getting document:", error);
-		});
-		window.alert(window.location.href);
-		if(window.location.href !="file:///D:/Programming%20etc/Shared%20Folder%20Ubuntu%20Main%2018.04.2/Luana/V1.3/login.html"){
-			mail = null;
-			window.alert("not on login and logged out. Redirecting to login page Email: " + mail);
-			window.location.replace ("login.html");
+	auth.onAuthStateChanged(function(user) {
+	  if (user) {
+		if(window.location.href =="file:///D:/Programming%20etc/Shared%20Folder%20Ubuntu%20Main%2018.04.2/Luana/V1.3/login.html"){
+		mail = user.email;
+		window.alert("on login logged in. redirecting to index User Email: " + mail);
+		window.location.replace ("index.html");
 
 		}
-	}
-});
+	  } else {
+
+			window.alert(window.location.href);
+			if(window.location.href !="file:///D:/Programming%20etc/Shared%20Folder%20Ubuntu%20Main%2018.04.2/Luana/V1.3/login.html"){
+				mail = null;
+				window.alert("not on login and logged out. Redirecting to login page Email: " + mail);
+				window.location.replace ("login.html");
+
+			}
+		}
+	});
 
 
-function logOut(){
-	auth.signOut().then(function(){
-	window.alert("pressed log out button");
-	return true;
+	function logOut(){
+		auth.signOut().then(function(){
+		window.alert("pressed log out button");
+		return true;
 
-	}).catch(function(error) {
+		}).catch(function(error) {
+			var errorCode = error.code;
+			var errorMessage = error.message;
+			window.alert("Error : " + errorMessage);
+
+		});
+	};
+	$("button#btnLogOut").on("click", logOut);
+
+
+	function logIn(){
+		window.alert("pressed log in button");
+		var userEmail = document.getElementById("txtEmail").value;
+		var userPass = document.getElementById("txtPassword").value;
+
+		auth.signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+		// Handle Errors here.
 		var errorCode = error.code;
 		var errorMessage = error.message;
 		window.alert("Error : " + errorMessage);
-
-	});
-};
-
-
-
-function logIn(){
-	window.alert("pressed log in button");
-	var userEmail = document.getElementById("txtEmail").value;
-	var userPass = document.getElementById("txtPassword").value;
-
-	auth.signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-	// Handle Errors here.
-	var errorCode = error.code;
-	var errorMessage = error.message;
-	window.alert("Error : " + errorMessage);
-	});
-};
-
+		});
+	};
+	$("button#btnLogIn").on("click", logIn);
+	
+})(jQuery);
 
 
--- V1.3/assets/js/main.js
+++ V1.3/assets/js/main.js
@@ -1,7 +1,92 @@
 
 (function($) {
+	const auth = firebase.auth();
+	const db = firebase.firestore();
+	const col = db.collection("items");
+	var mail;
+	var query;
+	var start;
+	const $row = $("section>.row.aln-center");
+	auth.onAuthStateChanged(function(user) {
+		if (user) {
+			mail = user.email;
+			//query = col.where("authorMail", "<", mail).where("authorMail", ">", mail);
+			//window.alert(query);
+			start = 1;
+		}
+	   else {
+			//not logged in
+		}
+	});
+	function addItem(doc, description, price){
+		console.log($row);
+		$(											
+			'<div class="col-8 col-10-medium col-16-small">' + 
+				'<section class="box">' +
+					'<a href="#" class="image featured"><img src="images/weisserStoff.jpg" alt="" /></a>' + 
+					'<header>' + 
+						'<h3>Der weiße Stoff</h3>' + 
+					'</header>' + 
+					'<p>' + description + '</p>' + 
+					'<b>Preis: ' + price + ' Euro/M^2</b>' + 
+					'<footer>' + 
+						'<ul class="actions">' + 
+							'<li><a href="#" class="button alt">Find out more</a></li>' + 
+						'</ul>' + 
+					'</footer>' + 
+				'</section>' + 
+			'</div>'
+		)
+		.appendTo($row);
+		
+	};
+	setInterval(function(){
+		//var docRef = query.doc("House 1");
+		if(start){
+			db.collection("items").get().then((snapshot) => {
+				snapshot.docs.forEach(doc => {
+					addItem(doc, doc.data().description, doc.data().price);
+				})
+				
+				
+			});
+			/*db.collection("items").where("authorMail", "<", mail).where("authorMail", ">", mail).get().then((snapshot) => {
+				console.log(snapshot.docs);
+				
+				
+			});*
+			
+			/*docRef.get().then(function(doc) {
+				if(doc.exists) {
+					console.log("Document data:", doc.data());
+				} else {
+					console.log("No such document!");
+				}
+			}).catch(function(error) {
+				console.lgo("Error getting document: ", error);
+				
+			});*/
+			//window.alert(query);
+			start = 0;
+		
+	}}, 1);
 
-	
+/*		docRef.get().then(function(doc) {
+		if (doc.exists) {
+			console.log("Document data:", doc.data().price);
+		} else {
+			// doc.data() will be undefined in this case
+			console.log("No such document!");
+		}
+	}).catch(function(error) {
+		console.log("Error getting document:", error);
+	});
+	*/	
+		
+		
+		
+		
+		
 	var	$window = $(window),
 		$body = $('body');
 
@@ -21,22 +106,24 @@
 		});
 
 	// Dropdowns.
-		$('#nav > ul').dropotron({
-			mode: 'fade',
-			noOpenerFade: true,
-			alignment: 'center'
-		});
-
+		if(typeof $('#nav > ul').dropotron !== "undefined") {
+			$('#nav > ul').dropotron({
+				mode: 'fade',
+				noOpenerFade: true,
+				alignment: 'center'
+			});
+		
 	// Nav.
 
 		// Title Bar.
+			
 			$(
 				'<div id="titleBar">' +
 					'<a href="#navPanel" class="toggle"></a>' +
 				'</div>'
 			)
 				.appendTo($body);
-
+		
 		// Panel.
 			$(
 				'<div id="navPanel">' +
@@ -59,5 +146,5 @@
 					target: $body,
 					visibleClass: 'navPanel-visible'
 				});
-
+		}
 })(jQuery);
